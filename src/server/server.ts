0309ef4f906013d3866c8c439a4a9c6a3f61b2b1
TienDVD <doanvanduytien@gmail.com>
2026-06-23 00:07:30 +0700

    update

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,11 @@
 import Express from "express";
+import cors from 'cors';
 
 const app = Express();
 
 const PORT = 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
